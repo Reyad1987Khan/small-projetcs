4e5025f7fbbc500eeb86c789bbdf91adc8b3e5aa
Reyad Khan <khanreyad207@gmail.com>
2023-06-15 02:26:08 +0600

    edit and list file create and commit

---
+++ Product_edit.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Product</title>
+    <link rel="stylesheet" href="project01.css">
+</head>
+<body>
+<script src="project01.js"></script>
+</body>
+</html>
